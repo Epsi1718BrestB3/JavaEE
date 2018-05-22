ffb05e7da7bca131b1e9494ad686b3e4854f9e41
Andy <Andy@DESKTOP-P8ARMQS>
2018-05-22 10:01:18 +0200

    classe controleur finale

--- src/fr/epsi/b3/controller/ControllerImpl.java
+++ src/fr/epsi/b3/controller/ControllerImpl.java
@@ -4,11 +4,15 @@ import fr.epsi.b3.model.Moteur;
 import fr.epsi.b3.model.Score;
 
 public class ControllerImpl implements Controller {
-	private Moteur moteur;
-	 public ControllerImpl (){
-		 moteur = new Moteur();
-	 }
 	
+	private Moteur moteur;
+	private String message;
+	
+	
+	public ControllerImpl (){
+		moteur = new Moteur();
+	}
+
 	@Override
 	public Score recupererScore() {
 		return moteur.getScore();
@@ -16,18 +20,24 @@ public class ControllerImpl implements Controller {
 
 	@Override
 	public String genererMessage() {
-		String message = null;
-		
-		return null;
+		return message;
 	}
 
 	@Override
 	public boolean lancer(int zoneCiblee) {
 		
-		return moteur.lancer(zoneCiblee);
+		boolean resultatLancer = moteur.lancer(zoneCiblee);
+		
+		if (resultatLancer) {
+			message = "GOAL!";
+		}else {
+			message = "Arrêt du tir par le gardien !";
+		}
+		
+		return resultatLancer;
 
 	}
 
-	
+
 
 }
